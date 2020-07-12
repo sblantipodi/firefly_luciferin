30422f6f24a091fb2075627b362a4bf511202d33
Davide Perini <perini.davide@dpsoftware.org>
2020-07-12 17:14:08 +0200

    Release v0.2.1 - Java Packager API, Windows Installer

--- src/main/java/org/dpsoftware/FastScreenCapture.java
+++ src/main/java/org/dpsoftware/FastScreenCapture.java
@@ -75,7 +75,7 @@ public class FastScreenCapture {
     // GStreamer Rendering pipeline
     public static Pipeline pipe;
     public GUIManager tim;
-    public static final String VERSION = "0.2.0";
+    public static final String VERSION = "0.2.1";
 
 
     /**
