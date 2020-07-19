4ab203d697727c1298784c761c6157c217476bb4
Davide Perini <perini.davide@dpsoftware.org>
2020-07-19 16:26:21 +0200

    Switch to JavaFX

--- src/main/java/org/dpsoftware/GUIManager.java
+++ src/main/java/org/dpsoftware/GUIManager.java
@@ -28,7 +28,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.swing.*;
-import javax.swing.event.HyperlinkEvent;
 import java.awt.*;
 import java.awt.event.ActionListener;
 import java.io.IOException;
--- src/main/java/org/dpsoftware/InfoController.java
+++ src/main/java/org/dpsoftware/InfoController.java
@@ -1,3 +1,21 @@
+/*
+  InfoController.java
+
+  Copyright (C) 2020  Davide Perini
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy of
+  this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in
+  all copies or substantial portions of the Software.
+
+  You should have received a copy of the MIT License along with this program.
+  If not, see <https://opensource.org/licenses/MIT/>.
+*/
 package org.dpsoftware;
 
 import javafx.application.Platform;
@@ -7,16 +25,9 @@ import javafx.scene.control.Button;
 import javafx.scene.input.InputEvent;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
 public class InfoController {
 
     public Button closeButton;
-
-    @FXML
-    private void switchToSecondary() throws IOException {
-        FastScreenCapture.setRoot("secondary");
-    }
 
     @FXML public void onMouseClickedCloseBtn(InputEvent e) {
         Platform.setImplicitExit(false);
