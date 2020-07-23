225246a5f1df899525b567edbcd9efe75a282ba7
Davide Perini <perini.davide@dpsoftware.org>
2020-07-23 22:44:29 +0200

    Startup Option dialog

--- src/main/java/org/dpsoftware/gui/OptionsController.java
+++ src/main/java/org/dpsoftware/gui/OptionsController.java
@@ -25,6 +25,7 @@ import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Tooltip;
 import javafx.scene.input.InputEvent;
 import javafx.stage.Stage;
 
@@ -41,8 +42,12 @@ public class OptionsController {
     @FXML TextField numberOfThreads;
     @FXML public Button saveButton;
     @FXML public Button cancelButton;
+    @FXML ComboBox serialPort;
+    @FXML ComboBox aspectRatio;
 
-
+    /**
+     * Initialize controller with system's specs
+     */
     @FXML
     protected void initialize() {
 
@@ -55,8 +60,10 @@ public class OptionsController {
         scaling.setValue(((int) (screenInfo.getScaleX() * 100)) + "%");
         // Get screen resolution
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-        screenWidth.setText(String.valueOf(screenSize.width * scaleX));
-        screenHeight.setText(String.valueOf(screenSize.height * scaleY));
+        screenWidth.setText(String.valueOf((int) (screenSize.width * scaleX)));
+        addTextFieldListener(screenWidth);
+        screenHeight.setText(String.valueOf((int) (screenSize.height * scaleY)));
+        addTextFieldListener(screenHeight);
         // Init gamma
         gamma.getItems().addAll("1.8", "2.0", "2.2", "2.4");
         gamma.setValue("2.2");
@@ -64,28 +71,86 @@ public class OptionsController {
         captureMethod.getItems().addAll("DDUPL", "WinAPI", "CPU");
         captureMethod.setValue("DDUPL");
         // Init threads
-        numberOfThreads.setText("3");
-
-
-
+        numberOfThreads.setText("1");
+        addTextFieldListener(numberOfThreads);
+        // Serial ports, most OSs cap at 256
+        serialPort.getItems().add("AUTO");
+        for (int i=0; i<=256; i++) {
+            serialPort.getItems().add("COM" + i);
+        }
+        serialPort.setValue("AUTO");
+        // Aspect ratio
+        aspectRatio.getItems().addAll("FullScreen", "LetterBox");
+        aspectRatio.setValue("FullScreen");
+        // Form tooltips
+        setTooltips();
+        // Set focus on the save button
+        Platform.runLater(() -> saveButton.requestFocus());
 
     }
 
+    /**
+     * Save button event
+     * @param e event
+     */
     @FXML
     public void save(InputEvent e) {
 
         Platform.setImplicitExit(false);
-
         System.out.println("DA"+scaling.getValue());
 
     }
 
+    /**
+     * Cancel button event
+     * @param e event
+     */
     @FXML
     public void cancel(InputEvent e) {
 
         Platform.setImplicitExit(false);
+        final Node source = (Node) e.getSource();
+        final Stage stage = (Stage) source.getScene().getWindow();
+        stage.hide();
 
-        System.out.println("DA"+scaling.getValue());
+    }
+
+    /**
+     * Force TextField to be numeric
+     * @param textField numeric fields
+     */
+    void addTextFieldListener(TextField textField) {
+
+        textField.textProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue.matches("\\d*")) return;
+            textField.setText(newValue.replaceAll("[^\\d]", ""));
+        });
+
+    }
+
+    /**
+     * Set form tooltips
+     */
+    void setTooltips() {
+
+        Tooltip screenWidthTooltip = new Tooltip("Monitor resolution");
+        screenWidth.setTooltip(screenWidthTooltip);
+        Tooltip screenHeightTooltip = new Tooltip("Monitor resolution");
+        screenHeight.setTooltip(screenHeightTooltip);
+        Tooltip scalingTooltip = new Tooltip("OS scaling feature, you should not change this setting");
+        scaling.setTooltip(scalingTooltip);
+        Tooltip gammaTooltip = new Tooltip("Smaller values results in brighter LEDs but less accurate colors");
+        gamma.setTooltip(gammaTooltip);
+        Tooltip captureMethodTooltip = new Tooltip("If you have a GPU, Desktop Duplication API (DDUPL) is faster than other methods");
+        captureMethod.setTooltip(captureMethodTooltip);
+        Tooltip numberOfThreadsTooltip = new Tooltip("1 thread is enough when using DDUPL, 3 or more threads are recommended for other capture methods");
+        numberOfThreads.setTooltip(numberOfThreadsTooltip);
+        Tooltip serialPortTooltip = new Tooltip("AUTO detects first serial port available, change it if you have more than one serial port available");
+        serialPort.setTooltip(serialPortTooltip);
+        Tooltip aspectRatioTooltip = new Tooltip("LetterBox is recommended for films, you can change this option later");
+        aspectRatio.setTooltip(aspectRatioTooltip);
+        Tooltip saveButtonTooltip = new Tooltip("You can change this options later");
+        saveButton.setTooltip(saveButtonTooltip);
 
     }
 
