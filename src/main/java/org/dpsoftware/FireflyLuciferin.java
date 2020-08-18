ffb4ca75e9a0b65007e3406850ab46c95c7ccc35
Davide Perini <perini.davide@dpsoftware.org>
2020-08-18 03:00:55 +0200

    release v1.2.0

--- src/main/java/org/dpsoftware/FireflyLuciferin.java
+++ src/main/java/org/dpsoftware/FireflyLuciferin.java
@@ -85,7 +85,7 @@ public class FireflyLuciferin extends Application {
     // MQTT
     MQTTManager mqttManager = null;
     // JavaFX scene
-    public static final String VERSION = "1.1.0";
+    public static final String VERSION = "1.2.0";
 
 
     /**
