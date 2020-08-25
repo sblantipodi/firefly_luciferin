e6b960854e044fed6a45f34b5a155f247a84e0a8
Davide Perini <perini.davide@dpsoftware.org>
2020-08-25 12:37:30 +0200

    String refactor

--- src/main/java/org/dpsoftware/config/Constants.java
+++ src/main/java/org/dpsoftware/config/Constants.java
@@ -20,7 +20,7 @@ package org.dpsoftware.config;
 
 public class Constants {
 
-	public static final String FIREFLY_LUCIFERIN_VERSION = "1.1.3";
+	public static final String FIREFLY_LUCIFERIN_VERSION = "1.1.2";
 
 	// Misc
 	public static final String FIREFLY_LUCIFERIN = "Firefly Luciferin";
