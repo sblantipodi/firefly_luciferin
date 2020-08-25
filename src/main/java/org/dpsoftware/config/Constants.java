60142744de0eec19c5324cd9a5c7b554e990be5c
Davide Perini <perini.davide@dpsoftware.org>
2020-08-25 22:36:42 +0200

    Linux support, auto update feature (#13)

--- src/main/java/org/dpsoftware/config/Constants.java
+++ src/main/java/org/dpsoftware/config/Constants.java
@@ -20,7 +20,7 @@ package org.dpsoftware.config;
 
 public class Constants {
 
-	public static final String FIREFLY_LUCIFERIN_VERSION = "1.2.5";
+	public static final String FIREFLY_LUCIFERIN_VERSION = "1.2.6";
 
 	// Misc
 	public static final String FIREFLY_LUCIFERIN = "Firefly Luciferin";
