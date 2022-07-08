11ec596a8a771daf11efbfef438c5c6761a33f8d
Davide Perini <perini.davide@dpsoftware.org>
2022-07-08 13:01:27 +0200

    first implementation of advanced color correction

--- src/main/java/org/dpsoftware/gui/TestCanvas.java
+++ src/main/java/org/dpsoftware/gui/TestCanvas.java
@@ -243,6 +243,7 @@ public class TestCanvas {
         DisplayManager displayManager = new DisplayManager();
         for (DisplayInfo displayInfo : displayManager.getDisplayList()) {
             if (index == FireflyLuciferin.config.getMonitorNumber()) {
+                CommonUtility.toJsonString(displayInfo);
                 if (NativeExecutor.isWindows()) {
                     stage.setX((displayInfo.getMinX() + (displayInfo.getWidth() / 2)) - Constants.COLOR_CORRECTION_XMARGIN_DIALOG);
                     stage.setY((displayInfo.getMinY() + displayInfo.getHeight()) - calculateColorManagementDialogY());
