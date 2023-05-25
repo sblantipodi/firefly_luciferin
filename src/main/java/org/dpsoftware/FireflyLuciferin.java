0afa8482e16aa155210a880ee0ca2bb9f7fa45e4
Davide Perini <perini.davide@dpsoftware.org>
2023-05-26 00:05:15 +0200

    trying an increased UDP buffer size

--- src/main/java/org/dpsoftware/FireflyLuciferin.java
+++ src/main/java/org/dpsoftware/FireflyLuciferin.java
@@ -244,6 +244,7 @@ public class FireflyLuciferin extends Application implements SerialPortEventList
      */
     private void setRuntimeLogLevel() {
         LoggerContext loggerContext = (LoggerContext) LoggerFactory.getILoggerFactory();
+        log.info("Log level=" + config.getRuntimeLogLevel());
         loggerContext.getLogger(Constants.LOG_LEVEL_ROOT).setLevel(Level.toLevel(config.getRuntimeLogLevel()));
     }
 
