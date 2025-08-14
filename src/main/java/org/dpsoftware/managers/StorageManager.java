272f01ec96557151b6f0a2a5baba50879e9f5a03
Davide Perini <perini.davide@dpsoftware.org>
2025-08-14 13:03:53 +0200

    release version

--- src/main/java/org/dpsoftware/managers/StorageManager.java
+++ src/main/java/org/dpsoftware/managers/StorageManager.java
@@ -412,7 +412,7 @@ public class StorageManager {
         writeToStorage = updatePrevious2124(config, writeToStorage); // Version < 2.12.4
         writeToStorage = updatePrevious2187(config, writeToStorage); // Version < 2.18.7
         writeToStorage = updatePrevious2226(config, writeToStorage, filename); // Version <= 2.22.6
-        writeToStorage = updatePrevious2234(config, writeToStorage); // Version <= 2.23.4
+        writeToStorage = updatePrevious2235(config, writeToStorage); // Version <= 2.23.5
         return writeToStorage;
     }
 
@@ -592,14 +592,14 @@ public class StorageManager {
     }
 
     /**
-     * Update configuration file previous than 2.23.4
+     * Update configuration file previous than 2.23.5
      *
      * @param config         configuration to update
      * @param writeToStorage if an update is needed, write to storage
      * @return true if update is needed
      */
-    private boolean updatePrevious2234(Configuration config, boolean writeToStorage) {
-        if (UpgradeManager.versionNumberToNumber(config.getConfigVersion()) < UpgradeManager.versionNumberToNumber("2.23.4")) {
+    private boolean updatePrevious2235(Configuration config, boolean writeToStorage) {
+        if (UpgradeManager.versionNumberToNumber(config.getConfigVersion()) < UpgradeManager.versionNumberToNumber("2.23.5")) {
             writeToStorage = updateMqttDiscoveryEntities(config, writeToStorage);
         }
         return writeToStorage;
